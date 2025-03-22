b03772c162afa67f86a2609842748a68a66cfe87
linporu <qazwsx123456guy@gmail.com>
2025-03-22 16:21:48 +0800

    revert: reset whole project

--- background.js
+++ background.js
@@ -1,30 +1,5 @@
-/**
- * Chrome Extension Template
- * 
- * Basic background script structure.
- */
-
-// Configuration
-const config = {
-  // Debug mode
-  debug: true
-};
-
-// Logging utility
-const logger = {
-  log: function(message) {
-    if (config.debug) {
-      console.log(`[Extension BG] ${message}`);
-    }
-  }
-};
-
-// Listen for messages from content script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  logger.log('Received message from content script');
-  logger.log(JSON.stringify(request));
-  
-  // Send a simple response
-  sendResponse({ success: true, message: 'Background script received your message' });
-  return true;
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
 });
